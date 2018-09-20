3b05b601a14ba1720ff9a8e47977cbb186992b01
Adam Lynch <alynch14@students.towson.edu>
2018-09-19 22:59:11 -0400

    Added code to the thing.

--- Assignments/src/Geometry/Package.java
+++ Assignments/src/Geometry/Package.java
@@ -1,6 +1,17 @@
 package Geometry;
 
 public class Package implements Measurable {
+    private int length;
+    private int width;
+    private int height;
+    private int weight;
+    private int surfaceArea;
+    private int volume;
+
+    public Package(){
+        
+    }
+
     @Override
     public String getMeasure() {
         return null;
--- Assignments/src/Geometry/WindSpeed.java
+++ Assignments/src/Geometry/WindSpeed.java
@@ -1,8 +1,30 @@
 package Geometry;
 
 public class WindSpeed implements Measurable {
+    private int speed;
+    private int distance;
+    private int timeInSeconds;
+
+    public WindSpeed(){
+        speed = 0;
+        distance = 0;
+        timeInSeconds = 0;
+    }
+
+    public WindSpeed(int d, int t){
+        distance = d;
+        timeInSeconds = t;
+        speed = distance/timeInSeconds;
+    }
+
+    public WindSpeed(int s, int d, int t){
+        speed = s;
+        distance = d;
+        timeInSeconds = t;
+    }
+
     @Override
     public String getMeasure() {
-        return null;
+        return String.format("The wind's current speed is %d", speed);
     }
 }
