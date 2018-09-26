05ef7fe1d5e64ea7e1fe7e81cab7b2c84e7a992e
Noah Day <sheltielover432@gmail.com>
2018-09-25 20:57:15 -0400

    Measurable
    Created classes for Measurable

--- Assignments/src/Geometry/Measurable.java
+++ Assignments/src/Geometry/Measurable.java
@@ -1,8 +1,38 @@
 package Geometry;
 
 public interface Measurable {
-    String getMeasure();
-    Measurable Rectangle();
-    Measurable Package();
-    Measurable WindSpeed();
+    public String getMeasure();
+}
+public class Rectangle implements Measurable {
+    Double weightOZ, weightLBS;
+    public void Rectangle(Double weightLBS, Double weightOZ) {
+        this.weightLBS = weightLBS;
+        this.weightOZ = weightOZ;
+    }
+    @Override
+    public String getMeasure() {
+        return "Weight: "+weightLBS+"lbs, "+weightOZ+"oz";
+    }
+}
+class Package implements Measurable {
+    Double length, width;
+    public Package(Double length, Double width){
+        this.length=length;
+        this.width=width;
+    }
+
+    @Override
+    public String getMeasure() {
+        return "Length: "+length+", Width: "+ width;
+    }
+}
+class WindSpeed implements Measurable{
+    Double speed;
+    public WindSpeed(Double speed){
+        this.speed=speed;
+    }
+    @Override
+    public String getMeasure() {
+        return "Speed: "+speed;
+    }
 }
