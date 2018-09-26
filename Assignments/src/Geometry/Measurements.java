498308e961018fe7f73e1b136ebdb5bd5402b02b
Noah Day <sheltielover432@gmail.com>
2018-09-26 13:37:04 -0400

    Measurements
    Updated the measurements class to make the if statements more management

--- Assignments/src/Geometry/Measurements.java
+++ Assignments/src/Geometry/Measurements.java
@@ -1,8 +1,30 @@
 package Geometry;
 
 public class Measurements {
-    public Measurements()
+    public String Measurements(Object m) throws UnsizedException
     {
-
+        Class name=m.getClass();
+        String string = null;
+        if (m instanceof StringBuffer) {
+            string= "16 characters";
+        }
+        else if (m instanceof Sized){
+            if(m instanceof StudentRecords){
+                string= " ";
+            }
+            else if(m instanceof SemesterSchedule){
+                string = " ";
+            }
+            else if(m instanceof Department){
+                string = "16 credits";
+            }
+            else if(m instanceof Sized && m instanceof Building){
+                string = "8560 square feet";
+            }
+        }
+        else {
+            throw new UnsizedException("ERROR");
+        }
+        return string;
     }
 }
