b8cdea2156622127512603dfea68ea11ed1fb6dc
Adam Lynch <alynch14@students.towson.edu>
2018-10-24 22:59:35 -0400

    Created both iterators for arrays and ArrayLists

--- Assignment_3/MenuIteratorExample/src/MenuIterator/Menu.java
+++ Assignment_3/MenuIteratorExample/src/MenuIterator/Menu.java
@@ -9,21 +9,81 @@ public class Menu {
     public static final int DESSERT = 3;
     public static final boolean HEART_HEALTHY = true;
     public static final boolean NOT_HEART_HEALTHY = false;
-    private List<MenuItem> items;
+    private MenuIterator iterator;
 
-    private class MenuIterator{
+    /* So the assignment confused me, but it seemed like Lin
+       recommended us use an inner class to create the MenuIterator
+       so that's what I did here.
+     */
+    private class ListIterator implements MenuIterator{
+        private List<MenuItem> myList;
+        private int index;
 
+        public ListIterator(List<MenuItem> list){
+            myList.addAll(list);
+            index = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (index+1)<myList.size();
+        }
+
+        @Override
+        public MenuItem next() {
+            index++;
+            return myList.get(index);
+        }
     }
 
-    public Menu(){
-        items = new ArrayList<>();
+    private class ArrayIterator implements MenuIterator{
+        private MenuItem[] array;
+        private int index;
+
+        public ArrayIterator(MenuItem[] newArray){
+            array = new MenuItem[newArray.length];
+            index = 0;
+            int i = 0;
+            for(MenuItem item : newArray){
+                array[i] = item;
+                i++;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (index+1)<array.length;
+        }
+
+        @Override
+        public MenuItem next() {
+            index++;
+            return array[index];
+        }
     }
 
     public void addItem(MenuItem item){
-        items.add(item);
     }
 
-    public void deleteItem(MenuIterator iter){
+    public void deleteItem(MenuIterator iter, MenuItem itemToDelete){
+        if(){
+
+        }
+    }
+
+    public void AllItemsIterator(){
+
+    }
+
+    public void itemIterator(int itemType){
+
+    }
+
+    public void heartHealthyIterator(){
+
+    }
+
+    public void priceIterator(){
 
     }
 }
---
+++ Assignment_3/MenuIteratorExample/src/MenuIterator/MenuIterator.java
@@ -0,0 +1,6 @@
+package MenuIterator;
+
+public interface MenuIterator {
+    public boolean hasNext();
+    public MenuItem next();
+}
