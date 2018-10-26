83d9e73453a0b980c16c6e13000698cdb20b6e37
Lynch <alynch14@students.towson.edu>
2018-10-26 15:53:34 -0400

    Finished up iterators and factory methods for accessing them.

--- Assignment_3/MenuIteratorExample/src/MenuIterator/Menu.java
+++ Assignment_3/MenuIteratorExample/src/MenuIterator/Menu.java
@@ -1,6 +1,7 @@
 package MenuIterator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Menu {
@@ -9,81 +10,201 @@ public class Menu {
     public static final int DESSERT = 3;
     public static final boolean HEART_HEALTHY = true;
     public static final boolean NOT_HEART_HEALTHY = false;
-    private MenuIterator iterator;
+    private List<MenuItem> originalListOfItems;
 
-    /* So the assignment confused me, but it seemed like Lin
-       recommended us use an inner class to create the MenuIterator
-       so that's what I did here.
-     */
-    private class ListIterator implements MenuIterator{
-        private List<MenuItem> myList;
+     /**
+      * Iterator that will iterate over all of the items sent to it.
+      */
+    private class AllItemsIterator implements MenuIterator{
+        private List<MenuItem> listOfItems;
         private int index;
 
-        public ListIterator(List<MenuItem> list){
-            myList.addAll(list);
+        public AllItemsIterator(){
+            listOfItems = new ArrayList<>();
+            index = 0;
+        }
+
+        public AllItemsIterator(List<MenuItem> newList){
+            listOfItems.addAll(newList);
+            index = 0;
+        }
+
+        public AllItemsIterator(MenuItem[] array){
+            listOfItems = new ArrayList<>();
+            listOfItems.addAll(Arrays.asList(array));
             index = 0;
         }
 
         @Override
-        public boolean hasNext() {
-            return (index+1)<myList.size();
+        public boolean hasNext(){
+            return (index+1)<listOfItems.size();
         }
 
         @Override
-        public MenuItem next() {
+        public MenuItem next(){
             index++;
-            return myList.get(index);
+            return listOfItems;
         }
     }
 
-    private class ArrayIterator implements MenuIterator{
-        private MenuItem[] array;
+    /**
+     * Iterator that iterates over items of the specific category that the
+     * user enters in.
+     */
+    private class ItemIterator implements MenuIterator{
+        private List<MenuItem> listOfItems;
         private int index;
 
-        public ArrayIterator(MenuItem[] newArray){
-            array = new MenuItem[newArray.length];
+        public ItemIterator(){
+            listOfItems = new ArrayList<>();
             index = 0;
-            int i = 0;
-            for(MenuItem item : newArray){
-                array[i] = item;
-                i++;
+        }
+
+        public ItemIterator(List<MenuItem> newList, int category){
+            for(MenuItem item : newList){
+                if(item.getCategory() == category){
+                    listOfItems.add(item);
+                }
             }
+            index = 0;
+        }
+
+        public ItemIterator(MenuItem[] array, int category){
+            listOfItems = new ArrayList<>();
+            for(int i = 0; i < array.length; i++){
+                if(array[i].getCategory() == category){
+                    listOfItems.add(array[i]);
+                }
+            }
+            index = 0;
         }
 
         @Override
-        public boolean hasNext() {
-            return (index+1)<array.length;
+        public boolean hasNext(){
+            return (index + 1) < listOfItems.size();
         }
 
-        @Override
-        public MenuItem next() {
+        @Override 
+        public MenuItem next(){
             index++;
-            return array[index];
+            return listOfItems.get(index);
         }
+    }
+
+    /**
+     * Iterator that separates the non-healthy items out of the list 
+     * and iterates over only the healthy ones.
+     */
+    private class HeartHealthyIterator implements MenuIterator{
+        private List<MenuItem> listOfItems;
+        private int index;
+
+        public HeartHealthyIterator(){
+            listOfItems = new ArrayList<>();
+            index = 0;
+        }
+
+        public HeartHealthyIterator(List<MenuItem> newList){
+            for(MenuItem item : newList){
+                if(item.getHealthy()){
+                    listOfItems.add(item);
+                }
+            }
+            index = 0;
+        }
+        
+        public HeartHealthyIterator(MenuItem[] array){
+            listOfItems = new ArrayList<>();
+            for(int i = 0; i < array.length; i++){
+                if(array[i].getHealthy()){
+                    listOfItems.add(array[i]);
+                }
+            }
+            index = 0;
+        }
+
+        @Override
+        public boolean hasNext(){
+            return (index + 1) < listOfItems.size();
+        }
+
+        @Override
+        public MenuItem next(){
+            index++;
+            return listOfItems.get(index);
+        }
+    }
+
+    /**
+     * Iterator that sorts out the items that are over the price ceiling that the user sets,
+     * and iterates only over the items that are less.
+     */
+    private class PriceIterator implements MenuIterator{
+        private List<MenuItem> listOfItems;
+        private int index;
+
+        public PriceIterator(){
+            listOfItems = new ArrayList<>();
+            index = 0;
+        }
+
+        public PriceIterator(List<MenuItem> newList, double ceiling){
+            for(MenuItem item : newList){
+                if(item.getPrice() < ceiling){
+                    listOfItems.add(item);
+                }
+            }
+            index = 0;
+        }
+
+        public PriceIterator(MenuItem[] array, double ceiling){
+            listOfItems = new ArrayList<>();
+            for(int i = 0; i < array.length; i++){
+                if(array[i].getPrice()<ceiling){
+                    listOfItems.add(array[i]);
+                }
+            }
+            index = 0;
+        }
+
+        @Override
+        public boolean hasNext(){
+            return (index + 1) < listOfItems.size();
+        }
+
+        @Override
+        public MenuItem next(){
+            index++;
+            return listOfItems.get(i);
+        }
+    }
+    //End of Iterators
+
+    public Menu(){
+        originalListOfItems = new ArrayList<>();
     }
 
     public void addItem(MenuItem item){
+
     }
 
     public void deleteItem(MenuIterator iter, MenuItem itemToDelete){
-        if(){
-
-        }
+        
     }
 
-    public void AllItemsIterator(){
-
+    public MenuIterator getAllItemsIterator(){
+        return new AllItemsIterator(originalListOfItems);
     }
 
-    public void itemIterator(int itemType){
-
+    public MenuIterator getItemIterator(int itemType){
+        return new ItemIterator(originalListOfItems, itemType);
     }
 
-    public void heartHealthyIterator(){
-
+    public MenuIterator getHeartHealthyIterator(){
+        return new HeartHealthyIterator(originalListOfItems);
     }
 
-    public void priceIterator(){
-
+    public MenuIterator getPriceIterator(double priceCeiling){
+        return new PriceIterator(originalListOfItems, priceCeiling);
     }
 }
--- Assignment_3/MenuIteratorExample/src/MenuIterator/MenuItem.java
+++ Assignment_3/MenuIteratorExample/src/MenuIterator/MenuItem.java
@@ -6,5 +6,31 @@ public class MenuItem {
     private int category;
     private double price;
 
+    public MenuItem(String name, boolean healthy, int category, double price){
+        this.name = name;
+        this.healthy = healthy;
+        this.category = category;
+        this. price = price;
+    }
 
+    public String getName(){
+        return name;
+    }
+
+    public boolean getHealthy(){
+        return healthy;
+    }
+
+    public int getCategory(){
+        return category;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    @Override
+    public String toString(){
+        return new String(name + " - " + price);
+    }
 }
