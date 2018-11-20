fa5b37c9083c09bd271f017957a96d765c1c96c8
Noah Day <sheltielover432@gmail.com>
2018-11-20 01:03:10 -0500

    TaxCalcs
    Updated the tax calculation files

---
+++ Assignments/Assignment_4/src/MassTaxComputation.java
@@ -0,0 +1,22 @@
+public class MassTaxComputation  extends TaxComputation {
+    @Override
+
+    public double computeTax(PurchasedItems items, ReceiptDate date) {
+        if((Boolean) taxHoliday(date)){
+            return 0;
+        }
+        else{
+            double tax = 0.0625 * items.getTotalCost();
+            return tax;
+        }
+    }
+
+    protected boolean taxHoliday(ReceiptDate date) {
+        if (date.equals("08/13") || date.equals("08/14")){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
