7662c78d153b699f9a3883da3d8806193d90bbfd
Noah Day <sheltielover432@gmail.com>
2018-11-20 11:05:35 -0500

    Updated menu
    Split different aspects of the menu into subclasses

--- Assignments/Assignment_4/src/Main.java
+++ Assignments/Assignment_4/src/Main.java
@@ -1,43 +1,70 @@
+import java.io.DataInput;
 import java.util.Date;
 import java.util.Scanner;
+
+import static java.awt.SystemColor.menu;
 
 public class Main {
 
     private static Scanner input;
 
     public static void main(String[] args) {
-        String code=null;
-        String item=null;
-        String price=null;
+        Scanner input = new Scanner(System.in);
         PurchasedItems items = null;
         ReceiptDate date = ReceiptDate.getDate();
-        PurchasedItems purchasedItems = new PurchasedItems();
         ReceiptFactory factory = new ReceiptFactory();
-        System.out.println("Enter an item? Y/N");
-        input = new Scanner(System.in);
-        while (!input.equals("N")){
-            System.out.println("Enter an item code for purchase. Enter None to quit");
-            Scanner inputCode = new Scanner(System.in);
-            code=inputCode.toString();
-            System.out.println("Enter an item for purchase. Enter None to quit");
-            Scanner inputItem = new Scanner(System.in);
-            item=inputCode.toString();
-            System.out.println("Enter a price for purchase. Enter None to quit");
-            Scanner inputPrice = new Scanner(System.in);
-            price=inputCode.toString();
-            StoreItem itemBought = new StoreItem(code, item, price);
-            purchasedItems.addItem(itemBought);
-            System.out.println("Enter another item? Y/N");
+        Receipt receipt = (Receipt) factory.getReceipt(items, date);
+        PurchasedItems purchasedItems = new PurchasedItems();
+        displayOptions();
+        int choice = 1;
+        while (choice != 0) {
+            System.out.println("What would you like to do?\n------------------------");
+            displayOptions();
+            choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    break;
+                case 2:
+                    enterItem(purchasedItems);
+                    break;
+                case 3:
+                    receipt.prtReceipt();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private static void enterItem(PurchasedItems purchasedItems) {
+            System.out.println("Enter an item? Y/N");
             input = new Scanner(System.in);
+            while (!input.equals("N")) {
+                System.out.println("Enter an item code for purchase. Enter None to quit");
+                Scanner inputCode = new Scanner(System.in);
+                String code = inputCode.toString();
+                System.out.println("Enter an item for purchase. Enter None to quit");
+                Scanner inputItem = new Scanner(System.in);
+                String item = inputCode.toString();
+                System.out.println("Enter a price for purchase. Enter None to quit");
+                Scanner inputPrice = new Scanner(System.in);
+                String price = inputCode.toString();
+                StoreItem itemBought = new StoreItem(code, item, price);
+                purchasedItems.addItem(itemBought);
+                System.out.println("Enter another item? Y/N");
+                input = new Scanner(System.in);
+            }
         }
 
-        // todo display all available products to user(to be implemented)
+    private static void displayOptions() {
+        System.out.println("Your options for matrix operations are:\n" +
 
-        // todo get all user selections(to be implemented)
+                "----------------------------------------\n" +
 
-//        while(input!=null){add items to the reciept }
-        Receipt receipt = (Receipt) factory.getReceipt(items, date);
-        receipt.prtReceipt();
+                "\n" +
+                "1 – Start New Receipt\n" +
+                "2 – Add Items\n" +
+                "3 – Display Receipt\n");
 
     }
 }
